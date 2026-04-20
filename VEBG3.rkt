e82d1e172d334d38ecf92c443defb31b0fe04129
unknown <jakanji@calpoly.edu>
2026-04-20 16:46:22 -0700

    added top-interp and tests

--- VEBG3.rkt
+++ VEBG3.rkt
@@ -19,5 +19,14 @@
     [(? real? n) (NumC n)]
     [(list '+ left right) (PlusC (parse left) (parse right))]
     [(list '* left right) (MultC (parse left) (parse right))]
-    [other (error 'parse "Arith expected valid syntax, got ~e" other)]))
+    [other (error 'parse "expected valid syntax, got ~e" other)]))
 
+;;takes an s-expression and calles parser and interp
+(define (top-interp [expr : Sexp]) : Real (interp (parse expr)))
+
+
+
+;;top-interp tests
+(check-equal? (top-interp '(* (+ 1 1) 2)) 4)
+;;parse tests
+(check-exn #rx"parse: expected valid syntax, got 'a" (lambda () (parse 'a)))
