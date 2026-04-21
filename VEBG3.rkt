088632de3f032d033ce1a851bad8494dd581d000
unknown <jakanji@calpoly.edu>
2026-04-20 22:19:29 -0700

    added binops

--- VEBG3.rkt
+++ VEBG3.rkt
@@ -1,29 +1,29 @@
 #lang typed/racket
 (require typed/rackunit)
 
-(define-type ExprC (U NumC PlusC MultC))
+(define-type ExprC (U NumC BinOp))
+(struct BinOp ([op : (U '+ '*)] [frst : ExprC] [snd : ExprC])
+  #:transparent)
 (struct NumC ([n : Real]) #:transparent)
-(struct PlusC ([left : ExprC] [right : ExprC]) #:transparent)
-(struct MultC ([left : ExprC] [right : ExprC]) #:transparent)
 
 ;; interpretation evaluation for VEBG language
 (define (interp [a : ExprC]) : Real
   (match a
     [(NumC n) n]
-    [(PlusC l r) (+ (interp l) (interp r))]
-    [(MultC l r) (* (interp l) (interp r))]))
+    [(BinOp o l r) (cond
+                     [(equal? o '+) (+ (interp l) (interp r))]
+                     [(equal? o '*) (* (interp l) (interp r))])]))
 
 ;;parse the given concret syntax into an AST
 (define (parse [prog : Sexp]): ExprC
   (match prog
     [(? real? n) (NumC n)]
-    [(list '+ left right) (PlusC (parse left) (parse right))]
-    [(list '* left right) (MultC (parse left) (parse right))]
+    [(list '+ left right) (BinOp '+ (parse left) (parse right))]
+    [(list '* left right) (BinOp '* (parse left) (parse right))]
     [other (error 'parse "expected valid syntax, got ~e" other)]))
 
 ;;takes an s-expression and calles parser and interp
 (define (top-interp [expr : Sexp]) : Real (interp (parse expr)))
-
 
 
 ;;top-interp tests
